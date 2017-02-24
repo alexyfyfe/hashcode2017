dec633c703a1c2e37331f3b8491a8ca70d708b87
john <wildfireone@gmail.com>
2017-02-24 11:41:45 +0000

    working ish

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * @Date:   06-Feb-172017
  * @Filename: index.js
 * @Last modified by:   john
-* @Last modified time: 23-Feb-172017
+* @Last modified time: 24-Feb-172017
  */
-var v,e,r,c,x;
+var v, e, r, c, x;
 var firstline = true;
 
 
@@ -31,48 +31,240 @@ lr.on('line', function(line) {
 
 lr.on('end', function() {
     // All lines are read, file is closed now.
-    console.log(fullPizza);
-    quickSolution();
+    console.log(videos.length);
+    console.log(latencyD + ":" + k_servers);
+    console.log(endpoint_cache.length);
+    console.log(request_descriptions.length);
+    bananas2();
+    console.log(finalCaches.length);
 
-    writeOutput(slices);
+    //quickSolution();
+
+    writeOutput2();
 });
+var videos = [];
+var endpoint_cache = [];
+var request_descriptions = [];
+var vidsection = false;
+var latencysection = false;
+var latencyD;
+var k_servers;
+var current_endpoint = [];
+var caches = [];
+
+function processLine(line) {
+    if (firstline) {
+        firstline = false;
+        v = parseInt(line.split(" ")[0]);
+        e = parseInt(line.split(" ")[1]);
+        r = parseInt(line.split(" ")[2]);
+        c = parseInt(line.split(" ")[3]);
+        x = parseInt(line.split(" ")[4]);
+        console.log("vids: " + v + " endpoints: " + e + " requests: " + r + " cache servers: " + c + " capacity: " + x);
+        vidsection = true;
+    } else if (vidsection) {
+        videos = line.split(' ');
+        vidsection = false;
+        latencysection = true;
+    } else if (latencysection) {
+        //console.log("latency");
+        if (endpoint_cache.length > e) {
+            endpointsection = false;
+            requestsection = true;
+        }
+        latencyD = parseInt(line.split(" ")[0]);
+        k_servers = parseInt(line.split(" ")[1]);
+        console.log(latencyD + ":" + k_servers);
+        latencysection = false;
+        endpointsection = true;
+    } else if (endpointsection) {
+        //console.log("endpoint");
+        //console.log(endpoint_cache.length);
+
+        if (endpoint_cache.length > e) {
+            latencysection = false;
+            endpointsection = false;
+            requestsection = true;
+        } else if (current_endpoint.length > k_servers - 1) {
+            endpoint_cache.push(current_endpoint);
+            current_endpoint = [];
+            latencyD = parseInt(line.split(" ")[0]);
+            k_servers = parseInt(line.split(" ")[1]);
+            console.log(latencyD + ":" + k_servers);
+        } else {
+            current_endpoint.push(line.split(' '));
+        }
 
 
+    } else if (requestsection) {
+        request_descriptions.push(line.split(' '));
+    } else {
+        //    var row = [];
+        //    for (var c = 0; c < columns; c++) {
+        //        row.push(line[c]);
+        //    }
+        //    fullPizza.push(row);
+        //    lineNo++;
+    }
+}
 
+var finalCaches = [];
+var finalCacheSizes = [];
+var videosadded = [];
 
-function processLine(line){
-  if (firstline) {
-      firstline = false;
-      v = parseInt(line.split(" ")[0]);
-      e = parseInt(line.split(" ")[1]);
-      r = parseInt(line.split(" ")[2]);
-      c = parseInt(line.split(" ")[3]);
-      x = parseInt(line.split(" ")[4]);
-      console.log("vids: " + v + " endpoints: " + e + " requests: " + r + " cacheservers: " + c + " capacity: ");
-  } else {
-      var row = [];
-      for (var c = 0; c < columns; c++) {
-          row.push(line[c]);
-      }
-      fullPizza.push(row);
-      lineNo++;
-  }
+function bananas2() {
+    //console.log(endpoint_cache);
+    //console.log(endpoint_cache);
+    request_descriptions.sort(sortRFunction);
+
+    for (var r = 0; r < request_descriptions.length; r++) {
+        var endpoint = request_descriptions[r][1];
+        var video = request_descriptions[r][0];
+        var latencyArray = endpoint_cache[endpoint];
+        latencyArray.sort(sort1Function);
+        var la = 0;
+        var cache = latencyArray[la][0];
+
+        while (la < latencyArray.length) {
+            //console.log(la);
+            cache = latencyArray[la][0];
+            //console.log("cache "+cache);
+            var currentcache = [];
+            if (!finalCacheSizes[cache]) {
+                finalCacheSizes[cache] = 0
+            };
+            //console.log(parseInt(finalCacheSizes[cache])+":"+parseInt(videos[video]));
+            if(videosadded.length > 5000){
+              break;
+            }
+            if (videosadded.indexOf(video) > -1) {
+                break;
+            } else {
+                if (parseInt(finalCacheSizes[cache]) + parseInt(videos[video]) < x) {
+                    if (finalCaches[cache]) {
+                        finalCaches[cache] = finalCaches[cache] + (video + " ");
+                        videosadded.push(video);
+                        console.log(videosadded.length);
+
+                    } else {
+                        finalCaches[cache] = (video + " ");
+                        videosadded.push(video);
+                        console.log(videosadded.length);
+                    }
+                    finalCacheSizes[cache] = parseInt(finalCacheSizes[cache]) + parseInt(videos[video]);
+                    //console.log(finalCaches);
+                    break;
+                } else {
+                    la++;
+                }
+            }
+        }
+    }
 }
 
 
 
-function writeOutput(slices) {
+
+
+
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    } else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
+}
+
+function sortRFunction(a, b) {
+    if (a[2] === b[2]) {
+        return 0;
+    } else {
+        return (a[2] < b[2]) ? -1 : 1;
+    }
+}
+
+function sort1Function(a, b) {
+    if (a[1] === b[1]) {
+        return 0;
+    } else {
+        return (a[1] < b[1]) ? -1 : 1;
+    }
+}
+
+function bananas() {
+    var vcount = 0;
+    var cx = 0;
+    var current = [];
+    var csize = 0;
+
+    current.push(0 + " ");
+    var vidsort = [];
+
+    for (var j = 0; j < videos.length; j++) {
+        var row = [];
+        row.push(videos[j]);
+        row.push(j);
+        vidsort.push(row);
+    }
+
+    //console.log(vidsort);
+    vidsort.sort(sortFunction);
+
+    while (vcount < v && cx < c) {
+
+        if ((csize + parseInt(vidsort[vcount][0])) > parseInt(x)) {
+            //console.log(current);
+            caches.push(current);
+            cx++;
+            current = [];
+            current.push(cx + " ");
+            csize = 0;
+
+        } else {
+            //console.log(vcount);
+            current.push(vidsort[vcount][1] + " ");
+            csize = parseInt(csize) + parseInt(vidsort[vcount][0]);
+        }
+        vcount++;
+    }
+}
+
+
+
+
+
+function writeOutput() {
     var logger = fs.createWriteStream(filename + ".out", {
         flags: 'a' // 'a' means appending (old data will be preserved)
     });
 
-    logger.write(slices.length.toString().trim()); // append string to your file
+    logger.write(caches.length.toString().trim()); // append string to your file
     logger.write("\n");
-    for (var i = 0; i < slices.length; i++) {
-        var str = slices[i].toString();
+    for (var i = 0; i < caches.length; i++) {
+        //console.log(caches[i]);
+        var str = caches[i].toString();
         str = str.replace(/,/g, '');
         logger.write(str.trim());
         logger.write("\n");
+    }
+    logger.end();
+}
+
+function writeOutput2() {
+    var logger = fs.createWriteStream(filename + ".out", {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+    });
+
+    logger.write(finalCaches.length.toString().trim()); // append string to your file
+    logger.write("\n");
+    for (var i = 0; i < finalCaches.length; i++) {
+        //console.log(caches[i]);
+        if (finalCaches[i]) {
+            var str = finalCaches[i].toString();
+            str = str.replace(/,/g, '');
+            logger.write(i + " " + str.trim());
+            logger.write("\n");
+        }
     }
     logger.end();
 }
